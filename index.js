f0fa6d1a86a23de0d4d406aff1da4947dc9cdffb
Tien <tmtien0103@gmail.com>
2023-04-04 14:28:29 +0700

    boostrap 4

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    return res.send('Hello World!');
-})
-
-app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
