f9c71ad97a862df6a204e7a9b55c83a2e9cc6f41
Tien <tmtien0103@gmail.com>
2023-04-10 20:14:53 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -27,11 +27,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+            app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Routes init
-route(app);
+            route(app);
 
-app.listen(port, () =>
-    console.log(`Example app listening on port http://localhost:${port}`),
-);
+                app.listen(port, () =>
+                    console.log(`Example app listening on port http://localhost:${port}`),
+                );
